265b82dddd7cec60743ca1c893942b8aa87abced
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-05-14 15:38:47 -0500

    Adding the main component to consume services

---
+++ src/Traits/ConsumesExternalServices.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace JuanDMeGon\Traits;
+
+use GuzzleHttp\Client;
+
+trait ConsumesExternalServices
+{
+    /**
+     * Send a request to any service
+     * @return string
+     */
+    public function makeRequest($method, $requestUrl, $queryParams = [], $formParams = [], $headers = [], $hasFile = false)
+    {
+        $client = new Client([
+            'base_uri' => $this->baseUri,
+        ]);
+
+        if (method_exists($this, 'resolveAuthorization')) {
+            $this->resolveAuthorization($queryParams, $formParams, $headers);
+        }
+
+        $bodyType = 'form_params';
+
+        if ($hasFile) {
+            $bodyType = 'multipart';
+
+            $multipart = [];
+
+            foreach ($formParams as $name => $contents) {
+                $multipart[] = ['name' => $name, 'contents' => $contents];
+            }
+        }
+
+        $response = $client->request($method, $requestUrl, [
+            'query' => $queryParams,
+            $bodyType => $hasFile ? $multipart : $formParams,
+            'headers' => $headers,
+        ]);
+
+        $response = $response->getBody()->getContents();
+
+        if (method_exists($this, 'decodeResponse')) {
+            $response = $this->decodeResponse($response);
+        }
+
+        if (method_exists($this, 'checkIfErrorResponse')) {
+            $this->checkIfErrorResponse($response);
+        }
+
+        return $response;
+    }
+}
